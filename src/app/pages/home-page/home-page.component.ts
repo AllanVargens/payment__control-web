9dddb632a22e3d5ca332a7b41bf1699c94f53b72
allan.vargens <allanvargens@gmail.com>
2025-01-01 16:17:58 -0300

    feat: cards and card bar implemented in home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [CardComponent ,NavbarComponent, RouterOutlet],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
